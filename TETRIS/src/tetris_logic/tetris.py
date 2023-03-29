773d57b7aeea27e771c059d0ee6e31af576fcb4e
Kalle Hahl <kalle.hahl@hotmail.com>
2023-03-29 03:03:12 +0300

    small changes, added method to store previous pieces and modified one method to notice piece collision

--- TETRIS/src/tetris_logic/tetris.py
+++ TETRIS/src/tetris_logic/tetris.py
@@ -9,8 +9,7 @@ class Tetris:
 
     def __init__(self):
         self.piece = None
-        self.map_pieces = [[0 for x in range(10)] for y in range(20)]
-
+        self.map_pieces = [[0 for x in range(10)] for i in range(20)]
     
     def new_piece(self):
         self.piece = Piece(160,40)
@@ -22,9 +21,14 @@ class Tetris:
             self.piece.x = old_x
     
     def move_down(self, y):
-        if self.piece.y == 800-40:
-            self.new_piece()
+        old_y = self.piece.y
         self.piece.y += y
+        if self.out_of_bounds():
+            self.piece.y = old_y
+            self.piece.landed = True
+            self.map_piece()
+            return
+             
 
     def rotate(self):
         old = self.piece.rotation
@@ -35,19 +39,19 @@ class Tetris:
     def out_of_bounds(self):
         coordinates = self.piece.piece_info()
         for coordinate in coordinates:
+            y = BLOCK*coordinate[1]+self.piece.y
             x = BLOCK*coordinate[0]+self.piece.x
-            print(x)
-            if x > WIDTH-BLOCK or x < 0:
+
+            if x > WIDTH-BLOCK or x < 0 or y > HEIGHT-BLOCK \
+                or self.map_pieces[y//BLOCK][x//BLOCK] != 0:
                 return True
             
         return False
     
-    """
     def map_piece(self):
         coordinates = self.piece.piece_info()
         for coordinate in coordinates:
             x = coordinate[0]+(self.piece.x//40)
             y = coordinate[1]+(self.piece.y//40)
-            self.map_pieces[y][x] = 1
+            self.map_pieces[y][x] = self.piece.color
         print(self.map_pieces)
-    """
