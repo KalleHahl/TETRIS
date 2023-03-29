edc292f2cd87d01320cbb2ac19f75c852c9d2db9
Kalle Hahl <kalle.hahl@hotmail.com>
2023-03-29 03:01:35 +0300

    added method to draw previous pieces

--- TETRIS/src/game.py
+++ TETRIS/src/game.py
@@ -22,18 +22,24 @@ class Game:
     def draw(self):
         self.screen.fill(BACKROUND)
         self.draw_grid()
+        self.draw_previous()
         self.draw_piece()
         pygame.display.flip()
 
     def update(self):
         self.tetris.move_down(BLOCK)
-       #pygame.time.wait(200)
+        if self.tetris.piece.landed == True:
+            self.tetris.new_piece()
+        pygame.time.wait(200)
 
     def draw_piece(self):
         
         piece_coordinates = self.tetris.piece.piece_info()
         for coordinate in piece_coordinates:
             pygame.draw.rect(self.screen, self.tetris.piece.color, (self.tetris.piece.x+coordinate[0]*BLOCK, self.tetris.piece.y+coordinate[1]*BLOCK,BLOCK,BLOCK),0,4)
+            pygame.draw.rect(self.screen, (0,0,0), (self.tetris.piece.x+coordinate[0]*BLOCK, self.tetris.piece.y+coordinate[1]*BLOCK,BLOCK,BLOCK),3,4)
+
+            
         
     def events(self):
         for event in pygame.event.get():
@@ -52,13 +58,23 @@ class Game:
             pygame.draw.line(self.screen, (LINE), (x, 0), (x, HEIGHT))
         for y in range(0, HEIGHT, BLOCK):
             pygame.draw.line(self.screen, (LINE), (0,y), (WIDTH, y))
+    
+    def draw_previous(self):
+        for y in range(20):
+            for x in range(10):
+                color = self.tetris.map_pieces[y][x]
+                if color == 0:
+                    continue
+                pygame.draw.rect(self.screen, color, (BLOCK*x,BLOCK*y,BLOCK,BLOCK),0,4)
+                pygame.draw.rect(self.screen, (0,0,0), (BLOCK*x,BLOCK*y,BLOCK,BLOCK),3,4)
+
          
     def loop(self):
         while not self.end:
             if self.tetris.piece == None:
                 self.tetris.new_piece()
             self.events()
-            #self.update()
+            self.update()
             self.draw()
             
             
