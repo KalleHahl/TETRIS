bf92b407c2e7340981ff9118f4d951f207a94b4f
Kalle Hahl <kalle.hahl@hotmail.com>
2023-03-31 15:08:43 +0300

    added pylint, invoke and autopep8, wrote tasks

--- TETRIS/tasks.py
+++ TETRIS/tasks.py
@@ -3,3 +3,11 @@ from invoke import task
 @task
 def start(ctx):
     ctx.run('python3 src/index.py', pty=True)
+
+@task
+def format(ctx):
+    ctx.run('autopep8 --in-place --recursive src')
+
+@task
+def pylint(ctx):
+    ctx.run('pylint src')
