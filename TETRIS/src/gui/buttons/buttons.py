791f8e6c39669f727c294c7cc4a765d4e3a4116d
Kalle Hahl <kalle.hahl@hotmail.com>
2023-04-06 02:38:09 +0300

    changed button text

--- TETRIS/src/gui/buttons/buttons.py
+++ TETRIS/src/gui/buttons/buttons.py
@@ -11,13 +11,13 @@ class Buttons:
         self.screen = screen
 
     def resume(self):
-        return self.template('Resume')
+        return self.template('Press space to resume!')
 
     def game_over(self):
-        return self.template('Game over')
-    
+        return self.template('Game over! Press space for new game!')
+
     def template(self, text):
-        font = pygame.font.Font('freesansbold.ttf', 32)
+        font = pygame.font.Font('freesansbold.ttf', 16)
         text_1 = font.render(text, True, (0, 0, 0), (255, 255, 255))
         rectangle = text_1.get_rect()
         rectangle.center = (WIDTH//2, HEIGHT//2)
