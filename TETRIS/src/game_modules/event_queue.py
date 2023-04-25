1f642bcf4faa66a6b6cb1b59d16a23e4d09a107b
Kalle Hahl <kalle.hahl@hotmail.com>
2023-04-25 20:07:00 +0300

    added function to increase speed

--- TETRIS/src/game_modules/event_queue.py
+++ TETRIS/src/game_modules/event_queue.py
@@ -4,10 +4,15 @@ import pygame
 class EventQueue:
 
     def __init__(self):
+        self.speed = 750
         self.delay = pygame.constants.USEREVENT + 1
-        pygame.time.set_timer(self.delay, 750)
+        pygame.time.set_timer(self.delay, self.speed)
         self.move_down_fast = pygame.constants.USEREVENT + 2
         pygame.time.set_timer(self.move_down_fast, 30)
 
     def get(self):
         return pygame.event.get()
+    
+    def set_speed(self):
+        self.speed = self.speed // 2
+        pygame.time.set_timer(self.delay, self.speed)
