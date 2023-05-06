f339a33822a90edcb92b9c67a150bb01856d4152
Kalle Hahl <kalle.hahl@hotmail.com>
2023-05-06 21:14:44 +0300

    changed method to return list of tuples and added docstring

--- TETRIS/src/repositories/user_scores.py
+++ TETRIS/src/repositories/user_scores.py
@@ -1,17 +1,35 @@
 class UserScores:
+    """Repository class for saving user scores to database
+    """
     def __init__(self, connection):
+        """Class constructor
+
+        Args:
+            connection (sqlite connection): connection to database
+        """
         self._connection = connection
 
     def find_top_scores(self):
+        """Method for finding top 3 scores from scores table
+
+        Returns:
+            list: list of tuples with username and score
+        """
         cursor = self._connection.cursor()
 
         cursor.execute("select username, score from scores order by score limit 3")
 
         scores = cursor.fetchall()
 
-        return scores
+        return [(score["username"], score["score"]) for score in scores]
 
     def create_score(self, player, score):
+        """Method for adding a score to database
+
+        Args:
+            player (string): player name
+            score (int): player score
+        """
         cursor = self._connection.cursor()
 
         cursor.execute('insert into scores (username, score) values (?,?)',
