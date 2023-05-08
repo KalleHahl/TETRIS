e5343166f62e0320beb08f91e89932f9752c33b7
Kalle Hahl <kalle.hahl@hotmail.com>
2023-05-08 23:48:34 +0300

    formatting

--- TETRIS/src/tests/scorerepo_test.py
+++ TETRIS/src/tests/scorerepo_test.py
@@ -2,6 +2,7 @@ import unittest
 from src.database_connection import get_data_base_connection
 from src.repositories.user_scores import UserScores
 
+
 class TestScoreRepo(unittest.TestCase):
     def setUp(self):
         self.score_repo = UserScores(get_data_base_connection())
@@ -23,9 +24,9 @@ class TestScoreRepo(unittest.TestCase):
         score1 = 300
         score2 = 200
         score3 = 100
-        self.score_repo.create_score(player1,score1)
-        self.score_repo.create_score(player2,score2)
-        self.score_repo.create_score(player3,score3)
+        self.score_repo.create_score(player1, score1)
+        self.score_repo.create_score(player2, score2)
+        self.score_repo.create_score(player3, score3)
         players = self.score_repo.find_top_scores()
         self.assertEqual(players[0], ('Kalle', 300))
         self.assertEqual(players[1], ('Timo', 200))
